693e5d76ecb13b0b4ce9e166a7798c0ff318d726
Steven Zych <stevezych@gmail.com>
2023-08-21 19:54:53 -0600

    changes to pending qc func

--- Functions/getPendingQC.js
+++ Functions/getPendingQC.js
@@ -1,17 +1,21 @@
+import next from "next";
 import {listItemsFunction} from "/Functions/listItemsFunction"
 
-function checkPreviousQC(lastQcDate, secondToLastQcDate){
-    const dateObject = new Date(lastQcDate);
-    if (dateObject.getMonth() === 8 || dateObject.getMonth() === 3 || dateObject.getMonth() === 5 || dateObject.getMonth() === 10){
-        if (30 - dateObject.getDate() < 7){
-            return true;
-        }
-    }else if (31 - dateObject.getDate() < 7){
+function checkPreviousQC(lastQc){
+    const lastQcDate = new Date(lastQc);
+    const currentDate = new Date();
+    currentDate.setHours(0,0,0,0)
+
+    const sevenDaysFromNow = new Date(currentDate)
+    sevenDaysFromNow.setDate(currentDate.getDate() + 7)
+
+    const nextQcDate = new Date(lastQcDate)
+    nextQcDate.setMonth(lastQcDate.getMonth() + 1)
+    if (currentDate <= nextQcDate && nextQcDate <= sevenDaysFromNow ){
         return true;
     }else{
         return false;
     }
-
 }
 
 export async function pendingQC(){
@@ -24,16 +28,11 @@ export async function pendingQC(){
     }else{
         for (let i = 0; i < currentItems.length; i++){
             let lastDateQcPerformed = currentItems[i].qualityControl.items[currentItems[i].qualityControl.items.length - 1].datePerformed;
-            if (currentItems[i].qualityControl.items.length === 0 ){
+            if (currentItems[i].currentQuantity > 0 && currentItems[i].qualityControl.items.length === 0 ){
                 needQC.push(currentItems[i]);
-            } else if (currentItems[i].qualityControl.items.length === 1 && checkPreviousQC(lastDateQcPerformed)){
+            } 
+            if (currentItems[i].currentQuantity > 0 && currentItems[i].qualityControl.items.length > 0 && checkPreviousQC(lastDateQcPerformed)){
                 needQC.push(currentItems[i]);
-            }
-            if (currentItems[i].qualityControl.items.length > 1){
-                let secondToLastDateQcPerformed = currentItems[i].qualityControl.items[currentItems[i].qualityControl.items.length - 2].datePerformed
-                if (currentItems[i].currentQuantity > 0 && checkPreviousQC(lastDateQcPerformed, secondToLastDateQcPerformed)){
-                    needQC.push(currentItems[i]);
-                }
             }
             
         return needQC;
