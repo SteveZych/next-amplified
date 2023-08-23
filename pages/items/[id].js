4a8a446a7748795baf745637f1564a2532e235bc
Steven Zych <stevezych@gmail.com>
2023-08-22 22:05:14 -0600

    adding new pages to display more info on items

---
+++ pages/items/[id].js
@@ -0,0 +1,16 @@
+//this page is for individual item
+// Icon for current quantity
+// icon for target quantity
+// icon for when next QC is due?
+//table for updates
+// table for quality control 
+// 
+
+export default function IndividualItem(){
+    
+    return(
+        <div>
+
+        </div>
+    )
+}
